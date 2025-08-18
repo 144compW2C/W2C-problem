2d7eb05d935eb122769ca804905f09e6d505a87b
koudaihirata <2230051@ecc.ac.jp>
2025-08-19 05:55:58 +0900

    add:react-router-domの追加&react-router-domをテストで実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+import Login from './pages/Login/page'
+import Home from './pages/Home/page'
+import Problem from './pages/Problem/page'
+import NotFound from './pages/NotFound/page'
+import { Header } from './stories/Header'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const MainLayout: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + リアクト</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <Header />
+      <Outlet />
+    </main>
   )
 }
 
-export default App
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/problem" element={<Problem />} />
+      </Route>
+      <Route path="/*" element={<NotFound />} />
+    </>,
+  ),
+)
+
+export const App: React.FC = () => (
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
---
+++ src/pages/Problem/page.tsx
@@ -0,0 +1,7 @@
+export default function Problem() {
+  return (
+    <>
+      <p>問題集ページ</p>
+    </>
+  )
+}
