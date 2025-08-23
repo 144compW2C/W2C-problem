0efbca2ab19bbe50b2193468f7b052ecd5db8e76
koudaihirata <2230051@ecc.ac.jp>
2025-08-24 07:13:22 +0900

    add:問題作成詳細ページの共通部分のデザイン完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './App.css'
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Outlet,
-  Route,
-  RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Outlet,
+    Route,
+    RouterProvider,
 } from 'react-router-dom'
 import Login from './pages/Login/page'
 import Home from './pages/Home/page'
@@ -16,31 +16,34 @@ import CreateProblem from './pages/CreateProblem'
 import CreateProblemDetail from './pages/CreateProblem/detail'
 
 const MainLayout: React.FC = () => {
-  return (
-    <main>
-      <Header />
-      <Outlet />
-    </main>
-  )
+    return (
+        <main>
+            <Header />
+            <Outlet />
+        </main>
+    )
 }
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/login" element={<Login />} />
-      <Route path="/" element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/problem" element={<Problems />} />
-        <Route path="/createProblem" element={<CreateProblem />} />
-        <Route path="/createProblem/:id" element={<CreateProblemDetail />} />
-      </Route>
-      <Route path="/*" element={<NotFound />} />
-    </>,
-  ),
+    createRoutesFromElements(
+        <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<MainLayout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/problem" element={<Problems />} />
+                <Route path="/createProblem" element={<CreateProblem />} />
+                <Route
+                    path="/createProblem/detail"
+                    element={<CreateProblemDetail />}
+                />
+            </Route>
+            <Route path="/*" element={<NotFound />} />
+        </>,
+    ),
 )
 
 export const App: React.FC = () => (
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 )
