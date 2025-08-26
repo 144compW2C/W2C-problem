8cccfc773c204ff5746b4e5c27e7213d6e64b6de
okonakona <2240057@ecc.ac.jp>
2025-08-26 13:45:46 +0900

    Header パスネーム

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,36 +2,77 @@ import styles from './style.module.css'
 import hamburgerIcon from '../../assets/hamburger.svg'
 import iconImg from '../../assets/icon.jpg'
 import { useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false)
-  const toggleHamburger = () => {
-    setIsOpen(!isOpen)
-  }
-  return (
-    <header>
-      <div className={styles.header}>
-        <img
-          src={hamburgerIcon}
-          className={styles.hamburger}
-          alt="ハンバーガー"
-          width={30}
-          height={30}
-          onClick={toggleHamburger}
-        />
-        <div className={styles.figure}>
-          <img src={iconImg} className={styles.icon} alt="アイコン画像" />
-        </div>
-      </div>
-      <div className={`${styles.nav} ${isOpen ? styles.hamburgerOpen : ''}`}>
-        <nav>
-          <ul>
-            <li className={styles.active}>ホーム</li>
-            <li>問題集</li>
-            <li>問題作成</li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  )
+    const [isOpen, setIsOpen] = useState(false)
+    const toggleHamburger = () => {
+        setIsOpen(!isOpen)
+    }
+    const pathname = useLocation()
+    return (
+        <header>
+            <div className={styles.header}>
+                <img
+                    src={hamburgerIcon}
+                    className={styles.hamburger}
+                    alt="ハンバーガー"
+                    width={30}
+                    height={30}
+                    onClick={toggleHamburger}
+                />
+                <div className={styles.figure}>
+                    <img
+                        src={iconImg}
+                        className={styles.icon}
+                        alt="アイコン画像"
+                    />
+                </div>
+            </div>
+            <div
+                className={`${styles.nav} ${isOpen ? styles.hamburgerOpen : ''}`}
+            >
+                <nav>
+                    <ul>
+                        <Link
+                            to={'/'}
+                            className={
+                                location.pathname === '/' ? styles.active : ''
+                            }
+                        >
+                            <li>ホーム</li>
+                        </Link>
+                        <Link
+                            to={'/problem'}
+                            className={
+                                location.pathname === '/problem'
+                                    ? styles.active
+                                    : ''
+                            }
+                        >
+                            <li>問題集</li>
+                        </Link>
+                        <Link
+                            to={'/createProblem'}
+                            className={
+                                location.pathname === '/createProblem'
+                                    ? styles.active
+                                    : ''
+                            }
+                        >
+                            <li>問題作成</li>
+                        </Link>
+                        <Link
+                            to={'#'}
+                            className={
+                                location.pathname === '#' ? styles.active : ''
+                            }
+                        >
+                            <li>運営管理</li>
+                        </Link>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
 }
