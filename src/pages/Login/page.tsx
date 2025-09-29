72989221006a5f3ce3b9ef5c502e1e8eb1ba7d81
koudaihirata <2230051@ecc.ac.jp>
2025-09-29 16:45:57 +0900

    add:ログインページ作成

--- src/pages/Login/page.tsx
+++ src/pages/Login/page.tsx
@@ -1,7 +1,42 @@
+import styles from './style.module.css'
+import logo from '../../assets/w2clogo.svg'
+import { Button } from '@/stories/Button'
+
 export default function Login() {
-  return (
-    <>
-      <p>Loginページ</p>
-    </>
-  )
+    return (
+        <>
+            <div className={styles.loginBg}>
+                <h1>
+                    <img src={logo} alt="W2Cロゴ" />
+                </h1>
+                <p className={styles.errorMes}>
+                    学校のメールアドレスを入力してください
+                </p>
+                <div className={styles.loginForm}>
+                    <form action="">
+                        <div className={styles.inputWrap}>
+                            <p>ログイン</p>
+                            <div className={styles.input}>
+                                <label>メールアドレス</label>
+                                <input
+                                    type="text"
+                                    placeholder="学籍番号@ecc.ac.jp"
+                                />
+                            </div>
+                            <div className={styles.input}>
+                                <label>パスワード</label>
+                                <input type="password" />
+                            </div>
+                        </div>
+                        <div className={styles.BtnWrap}>
+                            <Button label="ログインして進む" />
+                            <a href="#">
+                                <p>新規登録はこちら &gt;&gt;</p>
+                            </a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
 }
